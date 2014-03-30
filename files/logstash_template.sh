03cde8b51c0f64e94d3e6da06ee316867abd259b
Israel Calvete <icalvete@gmail.com>
2014-03-30 19:27:19 +0200

    Updating template

--- files/logstash_template.sh
+++ files/logstash_template.sh
@@ -4,29 +4,30 @@ curl -XPUT http://localhost:9200/_template/logstash_per_index -d '
 	"template" : "logstash*",
         "settings" : {
                 "index.store.compress.stored" : "true",
-                "index.cache.field.type" : "soft",
-                "index.query.default_field" : "@message"
+                "index.cache.field.type" : "soft"
         },
         "mappings" : {
                 "_default_" : {
                         "_all" : {"enabled" : false},
+			"_source": { "compress": true },
                         "properties" : {
                                 "@fields" : {
                                         "type" : "object",
                                         "dynamic": true,
                                         "path": "full",
                                         "properties" : {
-                                                "ldaperrnum" : { "type": "integer"}
+                                                "errnum" : { "type": "integer"}
                                         }
-                                }
-                        },
-                        "@message": { "type": "string", "index": "analyzed" },
-                        "@source": { "type": "string", "index": "not_analyzed" },
-                        "@source_host": { "type": "string", "index": "not_analyzed" },
-                        "@source_path": { "type": "string", "index": "not_analyzed" },
-                        "@tags": { "type": "string", "index": "not_analyzed" },
-                        "@timestamp": { "type": "date", "index": "not_analyzed" },
-                        "@type": { "type": "string", "index": "not_analyzed" }
+                                },
+
+				"@message": { "type": "string", "index": "analyzed" },
+				"@source": { "type": "string", "index": "not_analyzed" },
+				"@source_host": { "type": "string", "index": "not_analyzed" },
+				"@source_path": { "type": "string", "index": "not_analyzed" },
+				"@tags": { "type": "string", "index": "not_analyzed" },
+				"@timestamp": { "type": "date", "index": "not_analyzed" },
+				"@type": { "type": "string", "index": "not_analyzed" }
+                        }
                 }
         }
 }
